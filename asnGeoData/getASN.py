93db71fdc467495b7c0b1cc2a845c02e8f60e1f6
Tarthir <tylerbrady34@gmail.com>
2019-01-15 11:05:13 -0700

    Made things a bit more generic so that the getASN.py and p0fer.py could be used better outside of the context of this project

--- asnGeoData/getASN.py
+++ asnGeoData/getASN.py
@@ -1,44 +1,49 @@
 import re
 import os
+import sys
 
-
-#this script will go through all the logfiles and search for all client ips
+# this script will go through all the logfiles and search for all client ips
 # we will then do ASN lookups on them from a batch file and a netcat command
 # that data will then be stored in this same directory
+if len(sys.argv) != 3:
+    print("Usage is getASN.py <asnData.asn path> <location of p0f log files>")
+    sys.exit(1)
 
-asnData='/home/tyler/data_gathering/asnGeoData/asnData.asn' 
-#asnFinalData = '/home/tyler/asnData/finalASNData.data'
-#def write_addr(type_of_addr):
-#get all ipv4
+logFilesLoc = sys.argv[3]
+asnData = sys.argv[2]  # '/home/tyler/data_gathering/asnGeoData/asnData.asn'
+# asnFinalData = '/home/tyler/asnData/finalASNData.data'
+# def write_addr(type_of_addr):
+# get all ipv4
 cnt = 0
 ip_set = {}
 ip_set = set()
-
-os.chdir("/home/tyler/data_gathering/p0f/p0fdata/")
-#we have to put 'begin' at the start of the file for the netcat call to work for ASN lookup
-with open(asnData,'w') as tmp:
+os.chdir(logFilesLoc)
+# os.chdir("/home/tyler/data_gathering/p0f/p0fdata/")
+# we have to put 'begin' at the start of the file for the netcat call to work for ASN lookup
+with open(asnData, 'w') as tmp:
     tmp.write("begin\n")
 
-for file in os.listdir("/home/tyler/data_gathering/p0f/p0fdata"):
-    #print(file)
+for file in os.listdir(logFilesLoc):
+    # print(file)
     if file.endswith(".p0f"):
-        #lets go through all the p0f files and get the IP addresses we find
-        with open(file,'r') as f:
+        # lets go through all the p0f files and get the IP addresses we find
+        with open(file, 'r') as f:
             for line in f:
-                ip_addr = re.search('cli=(.*?)/',line,re.DOTALL).group(1)
+                ip_addr = re.search('cli=(.*?)/', line, re.DOTALL).group(1)
                 if ip_addr:
                     ip_set.add(ip_addr)
 
-#Now that the ip addresses are in a set we will not use any duplicates
-with open(asnData,'a') as output:
+# Now that the ip addresses are in a set we will not use any duplicates
+with open(asnData, 'a') as output:
     for ip in ip_set:
         ip = ip + '\n'
         output.write(ip)
         cnt = cnt + 1
-#we have to put 'end' at the end of the file for ASN lookup to work
-with open(asnData,'a') as tmp:
+# we have to put 'end' at the end of the file for ASN lookup to work
+with open(asnData, 'a') as tmp:
     tmp.write("end\n")
 
 print('Found ' + str(cnt) + ' unique ip addresses')
 
-# after running this script the bash script will run: netcat whois.cymru.com 43 < asnPreProcessedData.data | sort -n > asnFinalData.data
+# after running this script the bash script will run:
+# netcat whois.cymru.com 43 < asnPreProcessedData.data | sort -n > asnFinalData.
--- gather_data.sh
+++ gather_data.sh
@@ -3,13 +3,14 @@ WD=$(pwd)
 home=/home/tyler/
 error=/home/tyler/data_gathering/data_gathering_errors
 printf "Gathering p0f data...\n"
-python $WD/p0f/p0fer.py $home $WD/p0f/unzipped_data/ 2> $error/p0fError.log
+# first argument here is where the .pcap files are coming from, it assumes /home/tyler/
+python $(WD)/p0f/p0fer.py $(home) $(WD)/p0f/unzipped_data/ $(WD)/p0f/p0fdata/ 2> $(error)/p0fError.log
 printf "P0f data gathered\n\n"
 printf "Gathering and ordering asn data...\n"
-python $WD/asnGeoData/getASN.py 2> $error/asnError.log
+python $(WD)/asnGeoData/getASN.py $(WD)/asnGeoData/asnData.asn $(WD)/p0f/p0fdata/ 2> $(error)/asnError.
 printf "Asn data ordered\n\n"
 printf "Getting full asn data now...\n"
-netcat whois.cymru.com 43 < $WD/asnGeoData/asnData.asn | sort -n > $WD/asnGeoData/asnFinalData.asn
+netcat whois.cymru.com 43 < $(WD)/asnGeoData/asnData.asn | sort -n > $(WD)/asnGeoData/asnFinalData.asn
 printf "Done! asnFinalData.data now holds all unique asn data\n\n"
 printf "Getting mult_ipv data...\n"
 
@@ -24,7 +25,7 @@ do
 	echo Please enter the full path to imaal.full
 	reader
 done
-python $WD/mult_ipv_data/getMultData.py $loc $WD/mult_ipv_data/ 2> $error/multError.log
+python $(WD)/mult_ipv_data/getMultData.py $loc $(WD)/mult_ipv_data/ 2> $(error)/multError.log
 printf "Compiled mult_ipv4.py and mult_ipv6.py data\n\n"
 
 printf "All data is held inside their respective directories\n"
@@ -40,8 +41,8 @@ case "$var" in
 	exit
 	;;
 esac
-cp $WD/asnGeoData/asnFinalData.asn $home/graphs/Plotting/datalib/
-cp $WD/p0f/p0fdata/*.p0f $home/graphs/Plotting/datalib/
-cp $WD/mult_ipv_data/*.ipv $home/graphs/Plotting/datalib/
+cp $(WD)/asnGeoData/asnFinalData.asn $(home)/graphs/Plotting/datalib/
+cp $(WD)/p0f/p0fdata/*.p0f $(home)/graphs/Plotting/datalib/
+cp $(WD)/mult_ipv_data/*.ipv $(home)/graphs/Plotting/datalib/
 echo Files moved!
 
